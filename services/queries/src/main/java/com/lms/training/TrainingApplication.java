670c73eba58b4f31305a3e10f975ba904ba4dd43
Paridhi Gupta <paridhi.gupta@ukg.com>
2024-07-11 14:35:40 +0530

    added eurekaserver

--- services/queries/src/main/java/com/lms/training/TrainingApplication.java
+++ services/queries/src/main/java/com/lms/training/TrainingApplication.java
@@ -3,8 +3,10 @@ package com.lms.training;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableFeignClients
 @OpenAPIDefinition
 @EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
 @SpringBootApplication
