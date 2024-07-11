3a692a1fb7f55a89e37a4eaec34342a146f8ea4a
Paridhi Gupta <paridhi.gupta@ukg.com>
2024-07-11 12:26:22 +0530

    added exceptions

--- services/courses/src/main/java/com/lms/training/TrainingApplication.java
+++ services/courses/src/main/java/com/lms/training/TrainingApplication.java
@@ -1,12 +1,8 @@
 package com.lms.training;
 
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@OpenAPIDefinition
-@EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
 @SpringBootApplication
 public class TrainingApplication {
 
