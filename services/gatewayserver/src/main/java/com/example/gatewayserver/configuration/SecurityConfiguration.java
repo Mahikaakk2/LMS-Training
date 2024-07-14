02e4730cedfadab275b774a3cc84cca3ff0eff43
Abhishek Pant <abhishek.pant@ukg.com>
2024-07-14 18:49:30 +0530

    added security feature for courses service

--- services/gatewayserver/src/main/java/com/example/gatewayserver/configuration/SecurityConfiguration.java
+++ services/gatewayserver/src/main/java/com/example/gatewayserver/configuration/SecurityConfiguration.java
@@ -1,12 +1,20 @@
 package com.example.gatewayserver.configuration;
 
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.convert.converter.Converter;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AbstractAuthenticationToken;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
+import org.springframework.security.oauth2.server.resource.authentication.ReactiveJwtAuthenticationConverterAdapter;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import reactor.core.publisher.Mono;
 
 @Configuration
 @EnableWebFluxSecurity
@@ -16,13 +24,23 @@ public class SecurityConfiguration {
     public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity httpSecurity) {
         httpSecurity.authorizeExchange(exchange -> exchange
                 .pathMatchers(HttpMethod.GET).permitAll()
-//                .pathMatchers(HttpMethod.POST).permitAll()
-                .pathMatchers("/lms/courses/**").authenticated()
-
-        ).oauth2ResourceServer(config -> config.jwt(Customizer.withDefaults()));
+                .pathMatchers(HttpMethod.POST,"/lms/courses/**").authenticated()
+                .pathMatchers(HttpMethod.PUT, "/lms/courses/**").authenticated()
+                .pathMatchers(HttpMethod.DELETE, "/lms/courses/**").authenticated()
+                .pathMatchers(HttpMethod.PATCH, "/lms/courses/**").authenticated()
+//                .pathMatchers("/synergybank/loans/**").hasRole("LOANS")
+//                .pathMatchers("/synergybank/cards/**").hasRole("CARDS")
+        ).oauth2ResourceServer(config -> config.jwt(spec -> spec.jwtAuthenticationConverter(grantedAuthorityExtractor())));
 
         httpSecurity.csrf(ServerHttpSecurity.CsrfSpec::disable);
 
         return httpSecurity.build();
     }
+
+    private Converter<Jwt, Mono<AbstractAuthenticationToken>> grantedAuthorityExtractor(){
+        JwtAuthenticationConverter jwtAuthenticationConverter = new JwtAuthenticationConverter();
+        jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(new KeyCloakRoleConverter());
+        return new ReactiveJwtAuthenticationConverterAdapter(jwtAuthenticationConverter);
+    }
+
 }
